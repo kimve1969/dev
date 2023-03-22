0bed6c77de2aa9427ef0f3dfc773d2ce7907ee41
kimve <kimve@debian-11.home.virtualbox.org>
2023-03-22 21:42:14 +0500

    add

--- cpp/book_one/exam-001-p61.cpp
+++ cpp/book_one/exam-001-p61.cpp
@@ -5,13 +5,51 @@
   see page 61 of book
 
   Character of processor of my notebook Lenovo is: 
-  proccessor Intel Core i5 frequance is 1.19 GHz
+  proccessor Intel Core i5 frequance is 1.19 GHz, Turbo mode is 3.6 GHz (4 core)
   TPP (Theoretical pick perfomance) = D (number of bytes) * F (frequance) = 1 bytes * 1.19E9 Hz = 1.19 GFLOPS
   This example perfomance = 0.38 GFLOPS, only 32% from TPP!
+
+  Character of processor of my Samsung Galaxy Tab S7, SM-T875
+  processor Qualcomm Snapdragon 865 Plus, 8 core, 3.1 GHz (1 core - 2.84 GHz, 3 core - 2.42 GHz, 4 core - 1.8 GHz)
+  TPP (for 1.8 GHz) = 1.8 GFLOPS
+  RAM 6144 Mb, LPDDR4 / 128 Gb
 */
 int main(int argc, char* argv[]){
-  int N=330'000'123;
-  double a = 1.0, result = 0.0, t;
+  int N=330'000'123, K =0;
+  double a = 1.0, result = 0.0, t = 0.0;
+
+  auto add = [&](const int K) constexpr{
+    result = 0.0;
+    t = omp_get_wtime();
+    {
+      double result_tmp[K]={0};
+      for(int i=0; i<N/K; ++i){
+	for(int j=0; j<K; ++j) result_tmp[j] += a; // compile with option -O3 unroll loop
+      }
+      
+      for(int i=0; i<N%K; ++i) result += a;
+      
+      for(int j=0; j<K; ++j) result += result_tmp[j];
+    }
+    t = omp_get_wtime()-t;
+    std::cout<<K<<". result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
+  };
+
+  add(1);
+  add(2);
+  add(4);
+  add(8);
+  add(16);
+  add(32);
+  add(48);
+  add(54);
+  add(58);
+  add(64);
+  add(128);
+  add(256);
+  std::cout<<"***********************\n";
+  
+  result = 0.0;
   t = omp_get_wtime();
   for(int i=0; i<N; ++i){
     result += a;
@@ -19,105 +57,20 @@ int main(int argc, char* argv[]){
   t = omp_get_wtime()-t;
   std::cout<<"1. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
 
-  result = 0.0;
   t = omp_get_wtime();
   {
     double result_tmp[2]={0,0};
     for(int i=0; i<N/2; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
+      //result_tmp[0] += a;
+      //result_tmp[1] += a;
+      for(int j=0; j<2; ++j) result_tmp[j] += a; // compile with option -O3 unroll loop
     }
     for(int i=0; i<N%2; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1];
+      
+    for(int j=0; j<2; ++j) result += result_tmp[j];
   }
   t = omp_get_wtime()-t;
   std::cout<<"2. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
-  result = 0.0;
-  t = omp_get_wtime();
-  {
-    double result_tmp[3]={0,0};
-    for(int i=0; i<N/3; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
-      result_tmp[2] += a;
-    }
-    for(int i=0; i<N%3; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1] + result_tmp[2];
-  }
-  t = omp_get_wtime()-t;
-  std::cout<<"3. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
-  result = 0.0;
-  t = omp_get_wtime();
-  {
-    double result_tmp[4]={0,0};
-    for(int i=0; i<N/4; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
-      result_tmp[2] += a;
-      result_tmp[3] += a;
-    }
-    for(int i=0; i<N%4; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1] + result_tmp[2] + result_tmp[3];
-  }
-  t = omp_get_wtime()-t;
-  std::cout<<"4. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
-  result = 0.0;
-  t = omp_get_wtime();
-  {
-    double result_tmp[5]={0,0};
-    for(int i=0; i<N/5; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
-      result_tmp[2] += a;
-      result_tmp[3] += a;
-      result_tmp[4] += a;
-    }
-    for(int i=0; i<N%5; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1] + result_tmp[2] + result_tmp[3] + result_tmp[4];
-  }
-  t = omp_get_wtime()-t;
-  std::cout<<"5. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
-  result = 0.0;
-  t = omp_get_wtime();
-  {
-    double result_tmp[6]={0,0};
-    for(int i=0; i<N/6; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
-      result_tmp[2] += a;
-      result_tmp[3] += a;
-      result_tmp[4] += a;
-      result_tmp[5] += a;
-    }
-    for(int i=0; i<N%6; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1] + result_tmp[2] + result_tmp[3] + result_tmp[4] + result_tmp[5];
-  }
-  t = omp_get_wtime()-t;
-  std::cout<<"6. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
-  result = 0.0;
-  t = omp_get_wtime();
-  {
-    double result_tmp[7]={0,0};
-    for(int i=0; i<N/7; ++i){
-      result_tmp[0] += a;
-      result_tmp[1] += a;
-      result_tmp[2] += a;
-      result_tmp[3] += a;
-      result_tmp[4] += a;
-      result_tmp[5] += a;
-      result_tmp[6] += a;
-    }
-    for(int i=0; i<N%7; ++i) result += a;
-    result += result_tmp[0] + result_tmp[1] + result_tmp[2] + result_tmp[3] + result_tmp[4] + result_tmp[5] + result_tmp[6];
-  }
-  t = omp_get_wtime()-t;
-  std::cout<<"7. result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-
   
   return 0;
 }
