e2e49646aee0b3c5594bada4b45984967ae85305
kimve1969 <kimve@mail.ru>
2023-04-03 18:19:26 +0000

    book_one

--- cpp/book_one/exam-002-p65.cpp
+++ cpp/book_one/exam-002-p65.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<iomanip>
 #include<omp.h>
-#include<emmintrin.h>
+#include<emmintrin.h> // under Intel CPU
+//#include<arm_neon.h> // under ARM CPU
 /*
   see page 65 of book, use intrinsic compile
   2.5.2 SIMD extention
@@ -16,33 +17,35 @@
   TPP (for 1.8 GHz) = 1.8 GFLOPS
   RAM 6144 Mb, LPDDR4 / 128 Gb
 */
-int N=330'000'123;
 
-template<int K> void tadd(){  
-    double a = 1.0, result = 0.0, t = 0.0;
-    double result_tmp[K];
+int main(int argc, char* argv[]){
+  std::cout<<"************************* ADD optimization by using instrinsics - SIMD  **********************************\n";
+  int N=330'000'123;
+
+  double a = 1.0, result = 0.0, t = 0.0;
+
+  {
+    __m128d result_tmp  = _mm_set_pd(0.0, 0.0);
+    __m128d aa = _mm_set_pd(a,a);
     
     t = omp_get_wtime();
     
-    for(int i=0; i<K; ++i) result_tmp[i]=0;
-      
-    for(int i=0; i<N/K; ++i){
-      for(int j=0; j<K; ++j) result_tmp[j] += a; // compile with option -O3 unroll loop
+    for(int i=0; i<N/2; ++i){
+      _mm_add_pd( result_tmp, aa); // using SIMD (single instruction multiple data) on FMA unit
     }
       
-    for(int i=0; i<N%K; ++i) result += a;
+    for(int i=0; i<N%*2; ++i) result += a;
       
-    for(int j=0; j<K; ++j) result += result_tmp[j];
+    result += ((double*)(&result_tmp))[0] + ((double*)(&result_tmp))[1];
     
     t = omp_get_wtime()-t;
     std::cout<<K<<". result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-}
+  }
 
-template<int K> void tadd_m128d(){  
-    double a = 1.0, result = 0.0, t = 0.0;
-    
-    __m128d result_tmp[K];
-    for(int i=0; i<K; ++i) result_tmp[i] = _mm_set_pd(0.0, 0.0);
+  /*result = 0.0;
+  {
+    __m128d result_tmp;
+    for(int i=0; i<2; ++i) result_tmp[i] = _mm_set_pd(0.0, 0.0);
 
     __m128d aa = _mm_set_pd(a,a);
     
@@ -58,17 +61,7 @@ template<int K> void tadd_m128d(){
     
     t = omp_get_wtime()-t;
     std::cout<<K<<". result: "<<std::setprecision(13)<<result<<std::setprecision(3)<<", time: "<<t<<" s, GFLOPS: "<<N/(t*1E9)<<"\n";
-}
-
-int main(int argc, char* argv[]){
-  
-  std::cout<<"************************* ADD optimization by using unrolling loop **********************************\n";
-  tadd<1>();
-  tadd<3>();
-  
-  std::cout<<"************************* ADD optimization by using instrinsics compiler as SIMD extension FMA unit **********************************\n";
-  tadd_m128d<1>();
-  tadd_m128d<3>();
+    }*/
   
   return 0;
 }
